279a1f00f62a8cb23c71ab8d4d1818073a9fd8df
subrat kumar pradhan <kumarsubratpradhan.10@gmail.com>
2022-05-14 23:28:12 +0530

    second commit

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app=express();
 app.get('/',(req,res)=>res.send('API is running!!'));
 
 const PORT=process.env.PORT||5000;
+//hello
 app.listen(PORT,()=>console.log(`server started on ${PORT}`))
 
